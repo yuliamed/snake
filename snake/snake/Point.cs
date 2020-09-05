61375cada3994022b1770c44bc1d0f15000988d1
yuliamed <yuliamed@list.ru>
2020-09-05 14:08:54 +0300

    Процедурный подход. Функция инкапсуляция

---
+++ snake/snake/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+    
+}
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,7 +6,12 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point(1,3,'*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+           
             Console.ReadLine();
         }
     }
